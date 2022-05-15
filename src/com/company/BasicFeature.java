0e1a7ca96c816ce9e262abf78de038c79c74fccb
MrASyntex <continentaldispatch@gmail.com>
2022-05-15 09:05:18 -0700

    The red car is ready with basic features

--- src/com/company/BasicFeature.java
+++ src/com/company/BasicFeature.java
@@ -2,6 +2,10 @@ package com.company;
 
 public class BasicFeature {
     String Engine ;
+String HeadLight;
+String doors;
+String Color;
+
 
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         BasicFeature car = new BasicFeature();
+        car.Color ="red" ;
+        car.doors="4";
+        car.Engine="V8";
+        car.HeadLight="blu";
+
+
     }
 }
